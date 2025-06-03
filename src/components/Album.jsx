ccd682675ce4daf1e0a6fb7ec60f132f374b4f2e
zoirovdev <abboszoirov17@gmail.com>
2025-06-03 17:09:25 +0500

    connect api to albums

--- src/components/Album.jsx
+++ src/components/Album.jsx
@@ -1,6 +1,31 @@
+import { useState, useEffect } from 'react'
+
 const Album = () => {
+  const [albums, setAlbums] = useState([])
+
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const response = await fetch('https://jsonplaceholder.typicode.com/albums')
+        const data = await response.json()
+	setAlbums(data)
+      } catch (error) {
+        console.error(error)
+      }
+    }
+
+    getData()
+  }, [])
+
+
   return (
-    <h1>Album</h1>
+    <div className="ml-[270px]">
+      {albums.map((album) => (
+	<div key={album.id} className="p-4">
+	  <p><strong>{album.title}</strong></p>
+	</div>
+      ))}
+    </div>
   )
 }
 
